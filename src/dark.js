19c3de6bbe61de3449a77ec0f1affa6243acb840
ahncid <ahncid@gmail.com>
2024-02-05 12:35:58 +0100

    git initialized

---
+++ src/dark.js
@@ -0,0 +1,8 @@
+document.getElementById("modeToggle").addEventListener("click", function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+});
---
+++ src/scrollDown.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollDown = document.querySelector(".fa-circle-down");
+  let currentScrollPosition =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  scrollDown.addEventListener("click", function () {
+    const viewportHeight = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight || 0
+    );
+
+    const scrollOffset = 90;
+
+    const nextPageY = currentScrollPosition + viewportHeight - scrollOffset;
+
+    window.scrollTo({
+      top: nextPageY,
+      behavior: "smooth", //
+    });
+
+    currentScrollPosition = nextPageY;
+  });
+});
